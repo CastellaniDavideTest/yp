f4bb95016ca16fbe36dc98f5d7d07aff8f935e00
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-02-14 14:06:53 +0100

    Created bin/yp.py

---
+++ bin/yp.py
@@ -0,0 +1,19 @@
+"""yp
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-2-14"
+
+class yp:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(yp.yp())
+	
+	def yp():
+		"""yp first funtion
+		"""
+		return "yp" #for the test (see test_yp.py)
+		
+if __name__ == "__main__":
+	yp()
